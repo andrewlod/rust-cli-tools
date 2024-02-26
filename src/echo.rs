c2975c0f440c52256c63c513c655f92c84f3f35b
Andre Wlodkovski <andremacwlod@gmail.com>
2024-02-26 11:51:11 -0300

    Add echo unit test

--- src/echo.rs
+++ src/echo.rs
@@ -1,6 +1,6 @@
 use std::{error::Error, io};
 
-pub fn echo(args: Vec<&String>, mut out: impl io::Write) -> Result<(), Box<dyn Error>> {
+pub fn echo<S: AsRef<str>>(args: Vec<S>, mut out: impl io::Write) -> Result<(), Box<dyn Error>> {
     let args_size = args.len();
 
     if args_size < 1 {
@@ -8,12 +8,13 @@ pub fn echo(args: Vec<&String>, mut out: impl io::Write) -> Result<(), Box<dyn E
     }
 
     for (i, arg) in args.iter().enumerate() {
+        let arg_ref = arg.as_ref();
         if i < args_size - 1 {
-            write!(&mut out, "{} ", arg)?;
+            write!(&mut out, "{} ", arg_ref)?;
             continue;
         }
 
-        write!(&mut out, "{}", arg)?;
+        write!(&mut out, "{}", arg_ref)?;
     }
 
     Ok(())
@@ -26,4 +27,21 @@ macro_rules! echo {
     ($args:expr) => {
         echo::echo($args, io::stdout())
     };
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_echo() {
+        let mut output = Vec::new();
+        let args = vec!["hello,", "world!"];
+        echo!(args, &mut output).expect("Command failed!");
+
+        let output = String::from_utf8(output).expect("Failed to convert to UTF-8!");
+        let expected_value = "hello, world!";
+
+        assert_eq!(output, expected_value);
+    }
 }
